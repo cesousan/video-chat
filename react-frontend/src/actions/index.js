a21cb2b89d2148bd79b51f9d122524492d42660c
Cesousan <cesar.sabater@tiny-atom.com>
2018-07-03 11:39:26 +0200

    idem

--- react-frontend/src/actions/index.js
+++ react-frontend/src/actions/index.js
@@ -1,14 +1,19 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER_INFO, FETCH_AUTH_TOKEN } from './types';
+
+// export const handleToken = token => async dispatch => {
+//   const res = await axios.post('/api/stripe', token);
+//
+//   dispatch({ type: FETCH_USER_INFO, payload: res.data });
+// };
+
+
+export const socialLogin = (socialMedia = 'facebook' | 'google') => async dispatch => {
+  const res = await axios.get(`/auth/${socialMedia}`);
+  dispatch({ type: FETCH_AUTH_TOKEN, payload: res.data });
+}
 
 export const fetchUser = () => async dispatch => {
   const res = await axios.get('/api/users/info');
-
-  dispatch({ type: FETCH_USER, payload: res.data });
-};
-
-export const handleToken = token => async dispatch => {
-  const res = await axios.post('/api/stripe', token);
-
-  dispatch({ type: FETCH_USER, payload: res.data });
+  dispatch({ type: FETCH_USER_INFO, payload: res.data });
 };
--- react-frontend/src/reducers/authReducer.js
+++ react-frontend/src/reducers/authReducer.js
@@ -1,8 +1,13 @@
-import { FETCH_USER } from '../actions/types';
+import {
+  FETCH_USER_INFO,
+  FETCH_AUTH_TOKEN
+} from '../actions/types';
 
 export default function(state = null, action) {
   switch (action.type) {
-    case FETCH_USER:
+    case FETCH_USER_INFO:
+      return action.payload || false;
+    case FETCH_AUTH_TOKEN:
       return action.payload || false;
     default:
       return state;
